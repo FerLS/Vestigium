20fd955cf3e4c428d5855e71a65e4f174e0783cc
Fernando Álvarez <114153352+FerLS@users.noreply.github.com>
2025-02-18 17:28:57 +0100

    gitgonore and utils

---
+++ src/utils/constants.py
@@ -0,0 +1,12 @@
+from enum import Enum
+
+class MovementDirections(Enum):
+    UP = 1
+    DOWN = 2
+    LEFT = 3
+    RIGHT = 4
+
+class MovementType(Enum):
+    IDLE = 1
+    WALK = 2
+    JUMP = 3    
---
+++ src/utils/image.py
@@ -0,0 +1,17 @@
+import pygame
+from pygame.locals import RLEACCEL
+import os
+
+def load_image(name: str, colorkey=None):
+    fullname = os.path.join("assets", name)
+    try:
+        image = pygame.image.load(fullname)
+    except pygame.error as message:
+        print("Cannot load image:", name)
+        raise SystemExit(message)
+    image = image.convert()
+    if colorkey is not None:
+        if colorkey is -1:
+            colorkey = image.get_at((0, 0))
+        image.set_colorkey(colorkey, RLEACCEL)
+    return image
