498056c70f7a74d142316ae288ebc3e16ea38b23
Fernando Álvarez <114153352+FerLS@users.noreply.github.com>
2025-03-10 15:34:14 +0100

    level

--- src/main.py
+++ src/main.py
@@ -6,18 +6,6 @@ from tkinter import messagebox
 
 if __name__ == "__main__":
 
-    messagebox.showwarning(
-        "Que guay!", "Acabas de instalar el nuevo Troyano 2.1, disfrutalo! :D"
-    )
-    messagebox.showinfo(
-        "Que poca seguridad!", "No deberias haber instalado el Troyano 2.1"
-    )
-    messagebox.showinfo(
-        "",
-        "Es coña, pero vigila que pulleas, que no es plan de que te metan un troyano de verdad\n"
-        "Besos, Fer",
-    )
-
     pygame.init()
     director = Director()
     first_scene = StartMenu(director)
