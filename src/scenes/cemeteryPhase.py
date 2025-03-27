25b803eb44452dd5125fecb342bf8aafeb96f53e
anabanobremtnz <ana.banobre@udc.es>
2025-03-27 21:18:29 +0100

    feat(cemetery): Full scenario without fireflies

--- src/scenes/cemeteryPhase.py
+++ src/scenes/cemeteryPhase.py
@@ -11,7 +11,6 @@ from entities.player import Player
 from enviorement.camera import Camera
 from entities.gravedigger import Gravedigger
 from entities.firefly import Firefly
-from entities.mushroom import Mushroom
 from trigger import Trigger, change_scene
 from scenes.fadeTransition import FadeTransition, FadeIn, FadeOut
 from light2 import ConeLight  # Import ConeLight
@@ -21,44 +20,76 @@ class CemeteryPhase(Phase):
     def __init__(self, director):
         super().__init__(director)
         self.screen = director.screen
+        self.camera = Camera(WIDTH, HEIGHT)
+        self.pressed_keys = {}
+        
+        area_rect = pygame.Rect(500, 500, 300, 200)
+        self.firefly = Firefly(600, 600, area_rect)
+        
+        self.lamppost_blink_timer = 0
+        self.lamppost_blink_state = 1
+
+        self.fade_out = FadeOut(self.screen, 1, on_complete= lambda: change_scene(self.director, "MinigamePhase"))        
+
+        self.instruction_text = None
+        self.instruction_timer = 0
+        
+        self.load_resources()
+        self.setup_groups()
+        self.setup_enemies()
+        self.setup_player()
+        self.setup_triggers()
+        self.setup_audio()
+        
+    def load_resources(self):
+        """
+        Load all resources needed for the scene.
+        """
         self.foreground = Tilemap("tiled/levels/test_level.tmx")
         self.resources = ResourceManager()
         self.sound_manager = SoundManager()
         self.background = Background(
             self.resources, "assets\\images\\backgrounds\\parallax_forest"
         )
-
-        self.camera = Camera(WIDTH, HEIGHT)
-        self.pressed_keys = {}
-        area_rect = pygame.Rect(500, 500, 300, 200)
-
-        self.firefly = Firefly(600, 600, area_rect)
-        self.mushroom = Mushroom(100, 800)
-        
+    
+    def setup_groups(self):
+        """
+        Setup the sprite groups and trigger list for the scene.
+        """
         self.lampposts_group = pygame.sprite.Group()
-        self.lights_group = pygame.sprite.Group(self.firefly.light, self.mushroom.light)
-        self.create_lampposts()
-        self.lamppost_blink_timer = 0
-        self.lamppost_blink_state = 1
-        self.mushrooms_group = pygame.sprite.Group(self.mushroom)
-        obstacles = [mushroom.platform_rect for mushroom in self.mushrooms_group]
-
-        spawn_coords = self.foreground.load_entity("player_spawn")
-        self.player = Player(spawn_coords.x, spawn_coords.y, self.foreground, obstacles)
-
-        self.sound_manager.play_music("mystic_forest.mp3", "assets\\music", -1)
-
-        self.fade_out = FadeOut(self.screen, 1, on_complete= lambda: change_scene(self.director, "MinigamePhase"))
-
-        # Triggers
+        self.lights_group = pygame.sprite.Group(self.firefly.light)
         self.triggers = []
+        
+    def setup_enemies(self):
+        """
+        Setup the entities for the scene.
+        """
+        self.create_lampposts()
+        
+    def setup_player(self):
+        """
+        Create the player entity.
+        """
+        spawn_coords = self.foreground.load_entity("player_spawn")
+        self.player = Player(spawn_coords.x, spawn_coords.y, self.foreground, obstacles = [])
+        
+    def setup_triggers(self):
+        """
+        Setup the triggers for the scene.
+        """
         self.init_trigger("cemetery_end", lambda: self.fade_out.start())
-
-        self.instruction_text = None
-        self.instruction_timer = 0
-
-    index = 0
-
+        if not self.director.restarted:
+                self.init_trigger("start_trigger", lambda: self.start_trigger())
+        else:
+            self.init_trigger("start_trigger", lambda: self.start_trigger())
+            self.init_trigger("start_again_trigger", lambda: self.start_again_trigger())
+    
+    def setup_audio(self):
+        """
+        Setup the audio for the scene.
+        """
+        self.sound_manager.play_music("mystic_forest.mp3", "assets\\music", -1)
+        
     def create_lampposts(self):
         """
         Create lampposts with associated ConeLight objects.
@@ -84,9 +115,6 @@ class CemeteryPhase(Phase):
         end_coords = self.foreground.load_entity(entity_name)
         self.end_phase_rect = pygame.Rect(end_coords.x, end_coords.y, end_coords.width, end_coords.height)
         self.triggers.append(Trigger(self.end_phase_rect, callback))
-        
-    def delete_trigger(self, entity_name):
-        self.triggers.remove(entity_name)
     
     def start_trigger(self):
         """
@@ -104,25 +132,16 @@ class CemeteryPhase(Phase):
 
     def update(self):
         dt = self.director.clock.get_time() / 1000
+        # Update entities
         self.player.update(self.pressed_keys, dt)
         self.firefly.update()
-        self.mushroom.update()
 
-        for mushroom in self.mushrooms_group:
-            if self.player.rect.colliderect(mushroom.platform_rect):
-                mushroom.glow = True
-                mushroom.bounce = True
-
-        if not self.director.restarted:
-                self.init_trigger("start_trigger", lambda: self.start_trigger())
-        else:
-            self.init_trigger("start_trigger", lambda: self.start_trigger())
-            self.init_trigger("start_again_trigger", lambda: self.start_again_trigger())
-
+        # Update triggers
         for trigger in self.triggers:
             trigger.check(self.player.rect)
             trigger.update(dt)
 
+        # Lamppost blink
         self.lamppost_blink_timer += dt
         for light in self.lampposts_group:
             adjusted_timer = (self.lamppost_blink_timer - light.blink_offset) % 6
@@ -135,32 +154,43 @@ class CemeteryPhase(Phase):
             elif 4 <= adjusted_timer < 6:  # Stay off
                 light.intensity = 0
 
+        # Update lights
         for light in self.lampposts_group:
             light.update(dt)
+            
+        # Collision detection with delay
+        in_collision = any(light.intensity > 0 and self.player.check_pixel_perfect_collision(light) for light in self.lights_group)
 
+        if in_collision:
+            self.collision_delay_timer += dt  # Start counting
+            if self.collision_delay_timer >= 0.25: 
+                self.player.is_dying = True
+        else:
+            self.collision_delay_timer = 0  # Reset timer if no collision
+        """
         for light in self.lights_group:
             if light.intensity > 0 and self.player.check_pixel_perfect_collision(light): 
                 self.player.is_dying = True
-
+        """
+        
+        # Restart if dead
         if self.player.dead:
             self.director.restarted = True
             self.director.scene_manager.stack_scene("CemeteryPhase")
         
         self.fade_out.update(dt)
-
         self.camera.update(self.player.rect)
 
         if self.instruction_timer > 0:
             self.instruction_timer -= dt
             if self.instruction_timer <= 0:
-                self.instruction_text = None  # Remove the text after 10 seconds
+                self.instruction_text = None
 
     def draw(self):
         offset = self.camera.get_offset()
 
         self.background.draw(self.screen, offset)
         self.foreground.draw(self.screen, offset)
-        #self.mushroom.draw(self.screen, offset)
         self.player.draw(self.screen, camera_offset=offset)
         self.firefly.draw(self.screen, offset)
         
